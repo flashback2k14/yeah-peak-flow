8f7cc17e6f81eac3f9ab2c1d53ca67062dabaf50
Sebastian Kloppe <yeahdev@gmail.com>
2026-03-25 20:27:33 +0100

    remove all

--- backend/src/utils/async-handler.ts
+++
@@ -1,9 +0,0 @@
-import type { NextFunction, Request, Response } from 'express';
-
-export const asyncHandler = (
-  handler: (req: Request, res: Response, next: NextFunction) => Promise<void>
-) => {
-  return (req: Request, res: Response, next: NextFunction): void => {
-    handler(req, res, next).catch(next);
-  };
-};
